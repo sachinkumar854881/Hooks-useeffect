7beadb6c323fed012c14eed7b640867ee59a44f3
sachin <sachinkumar854881@gmail.com>
2024-03-09 00:03:47 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState, useEffect} from 'react'
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter,setCounter] = useState(0)
+
+    const [count, setCount] = useState(0)
+
+    useEffect(()=>{
+        alert('working...')
+    },[counter])
+
+    function incre(){
+        setCounter(counter+1)
+    }
+    function increC(){
+        setCount(count+1)
+    }
+    return(
+        <center>
+            <div>
+                <h1>Welcome to useEffect🥇</h1>
+                <h1>Counter: {counter}  Count: {count}</h1>
+                <button onClick={incre}>Increase</button>
+                <button onClick={increC}>increase</button>
+            </div>
+        </center>
+    )
 }
-
 export default App;
